f91cb254d498ddb060ab523fa88937164d65493f
kapil <kapilbehwal9555@gmail.com>
2024-08-25 10:49:46 +0530

    all

--- components/About.js
+++ components/About.js
@@ -1,22 +1,15 @@
 'use client'
-import { Box, Container, Grid, IconButton, Menu, MenuItem, Typography } from '@mui/material'
-import React, { useState } from 'react'
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import { Box, Container, Grid, Typography } from '@mui/material';
 import Image from 'next/image';
-import foodstall from "@/assests/foodStall.png"
-import googleplay from "@/assests/playstore.png"
 // import Footer from './Footer';
-import food1 from "@/assests/img1.jpg"
-import food2 from "@/assests/img2.jpg"
-import food3 from "@/assests/img3.jpg"
-import food4 from "@/assests/food3.jpg"
-import food5 from "@/assests/img5.jpg"
+import food1 from "@/assests/img1.jpg";
+import food2 from "@/assests/img2.jpg";
+import food3 from "@/assests/img3.jpg";
+import food5 from "@/assests/img5.jpg";
 
 // import food1 from "../../public/food1.jpg"
 
-import QrCode2Icon from '@mui/icons-material/QrCode2';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link';
 
 const About = () => {
     const router = useRouter()
